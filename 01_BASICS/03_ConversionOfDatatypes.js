6b270d6488c0306359ef7ca3fcddecd3458dc36b
Aryan Chilakwad <aryanchilakwad1@gmail.com>
2026-06-07 18:44:08 +0530

    Add files via upload

---
+++ 01_BASICS/03_ConversionOfDatatypes.js
@@ -0,0 +1,19 @@
+score = "33abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber) // NaN -> Not a Number 
+
+Number = 33
+console.log(typeof Number)
+let valueinstring = String(Number)
+console.log(typeof valueinstring)
+console.log(valueinstring)
+
+IsheAboveage = true
+let isAboveageInString = String(IsheAboveage)
+console.log(typeof isAboveageInString)
+console.log(isAboveageInString)
